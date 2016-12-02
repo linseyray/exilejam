e5787a09aa8eef9bc9ef99dfbf9da61a80cf1b8f
Linsey R <Linsey R>
2016-12-02 15:30:22 +0100

    Experience script

---
+++ ExilePrototype/Assets/Scripts/ExperienceBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperienceBehaviour : MonoBehaviour {
+
+	private SpriteRenderer spriteRenderer;
+
+	private bool fadeOut = false;
+	private float fadeVelocity = 0.0f;
+	private float fadeTime = 0.3f;
+
+	void Awake() {
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+	}
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (fadeOut) {
+			Color newColor = spriteRenderer.color;
+			newColor.a = Mathf.SmoothDamp(newColor.a, 0.0f, ref fadeVelocity, fadeTime);
+			spriteRenderer.color = newColor;
+			Debug.Log("fading...");
+		}
+		
+	}
+
+	public void Consume() {
+		fadeOut = true;
+	}
+}
